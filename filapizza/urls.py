522a6fac41a8482e30fdac1ba27ed5b2aec25e9b
Ariel Shalom <ariel3d12@gmail.com>
2025-05-23 13:14:40 -0300

    Atualização da Home page

--- filapizza/urls.py
+++ filapizza/urls.py
@@ -6,5 +6,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cliente/', include('cliente.urls')),
-    path('funcionario/', include('funcionario.urls')),  # <-- ADICIONE ESTA LINHA
+    path('funcionario/', include('funcionario.urls')), 
+    path('', include('home_pizzaria.urls')),
 ]
