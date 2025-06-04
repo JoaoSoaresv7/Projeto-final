ba8fd8a85744122c08b992b42579a205389c0e58
Rafael Ramos dos Santos <rafael.ramoss145@gmail.com>
2025-06-03 23:21:47 -0300

    MySQL
    Final

--- filapizza/settings.py
+++ filapizza/settings.py
@@ -80,8 +80,16 @@ WSGI_APPLICATION = 'filapizza.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pizzaria',
+        'USER': 'root',
+        'PASSWORD': 'Dall@145',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
